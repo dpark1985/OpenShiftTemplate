cc42424d259fe245c640e4062e66e7d6546c560c
Daniel Park <dpark1985@gmail.com>
2014-11-17 22:38:51 +0900

    OpenShift Template Project

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+});
+
+router.get('/testing', function(req, res) {
+  res.send('Testing user/testing');
+});
+
+
+module.exports = router;
